a23c73ab913b05f796f26a8039d2aa4321d2cd64
Hajk Dallakjan <hajk.dallakjan@bold.com>
2025-01-28 21:37:26 +0100

    implement supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  'https://fziildzidjrcaermdmfh.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6aWlsZHppZGpyY2Flcm1kbWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5OTM0NzcsImV4cCI6MjA1MzU2OTQ3N30.Uw_ak51NlSrIyybT0gcPrVGY7ShEAO_xSw1VWTHdk6E'
+);
+
+export default supabase;
